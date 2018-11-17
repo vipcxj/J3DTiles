8b6dc82813c20e63600bd0b40e0ed52e592c8561
vipcxj <vipcxj@126.com>
2018-11-17 18:45:55 +0800

    add a module to transform ifc file to 3dtiles format.

---
+++ j3dtiles-ifc-tools/src/main/java/me/cxj/j3dtiles/ifc/model/IfcModelChangeListener.java
@@ -0,0 +1,10 @@
+package me.cxj.j3dtiles.ifc.model;
+
+import org.bimserver.emf.IdEObject;
+
+/**
+ * Created by vipcxj on 2018/11/16.
+ */
+public interface IfcModelChangeListener {
+    void objectAdded(IdEObject idEObject);
+}
